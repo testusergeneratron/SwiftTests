e66748cb2d34b4d4ed365979c61fa1cd9da77105
Evol Johnson <ejohnson@ngeosone.com>
2016-03-24 20:07:24 -0400

    Initial-Commit

---
+++ MyProject/Sources/MyProject/main.swift
@@ -0,0 +1,58 @@
+/* 
+Copyright (c) 2016 NgeosOne LLC
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+   
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+ to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+Engineered using http://www.generatron.com/
+
+[GENERATRON]
+Generator :   System Templates
+Filename:     main.swift
+Description:  Swift Server
+Project:      MyProject
+Template: Kitura-Init/main.swift.vmg
+ */
+import KituraSys
+import KituraNet
+import KituraRouter
+
+import LoggerAPI
+import HeliumLogger
+
+#if os(Linux)
+    import Glibc
+#endif
+
+import Foundation
+
+#if os(OSX)
+    import Mustache
+#endif
+
+
+MyProjectRouter.sharedInstance.setupRoutes();
+ 
+#if os(OSX) // Mustache implemented for OSX only yet
+
+#endif
+
+let server = HttpServer.listen(Config.sharedInstance.serverPort,delegate: MyProjectRouter.sharedInstance)
+print("Server up and running on http://localhost:\(Config.sharedInstance.serverPort)/api/")
+Server.run()
+/* 
+[STATS]
+It would take a person typing  @ 100.0 cpm, 
+approximately 5.38 minutes to type the 538+ characters in this file.
+ */
